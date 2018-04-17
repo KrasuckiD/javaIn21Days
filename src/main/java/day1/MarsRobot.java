67302c991dc8dc5b2c84e845ff6483db5614c5c1
KrasuckiD <darasmm@gmail.com>
2018-04-17 13:15:52 +0200

    day1

--- src/main/java/day1/MarsRobot.java
+++ src/main/java/day1/MarsRobot.java
@@ -1,4 +1,20 @@
 package day1;
 
 public class MarsRobot {
+    String status;
+    int speed;
+    float temperature;
+
+    void checkTemperature(){
+        if (temperature < -62){
+            status = "return home";
+            speed = 5;
+        }
+    }
+
+    void showAttributes () {
+        System.out.println("Status: " + status);
+        System.out.println("Speed: " + speed);
+        System.out.println("Temperature " + temperature);
+    }
 }
